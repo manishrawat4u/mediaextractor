3539ebda086e96acd3981e84444731c31cb4b3e7
Manish Rawat <manish.rawat@nagarro.com>
2018-03-02 16:50:09 +0530

    Initial checkin

---
+++ index.js
@@ -0,0 +1,61 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var got = require('got');
+var cheerio = require('cheerio');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+function getRandomRolor() {
+    var lum = -0.25;
+    var hex = String('#' + Math.random().toString(16).slice(2, 8).toUpperCase()).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    var rgb = "#",
+        c, i;
+    for (i = 0; i < 3; i++) {
+        c = parseInt(hex.substr(i * 2, 2), 16);
+        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        rgb += ("00" + c).substr(c.length);
+    }
+    return rgb;
+}
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('message', async function (data) {
+        var tag = JSON.stringify(data);
+        var tagColor = getRandomRolor();
+        function sendToClient(message, url) {
+            socket.emit('message', {
+                tag: tag,
+                message: message,
+                url: url,
+                tagColor: tagColor
+            });
+        }
+        sendToClient('Recieved...');
+
+        var response = await got(data).catch(() => {
+            sendToClient('Error...');
+        });
+        if (response) {
+            var $ = cheerio.load(response.body);
+            $('a').each(function (i, link) {
+                sendToClient($(link).text(), $(link).attr('href'));
+            });
+            console.log('a message recvd...');
+            sendToClient('Completed...');
+        }
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
